36d71cbc0be093b9c55ab79ba6f3262dac53659f
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-04-30 12:50:37 -0400

    Generate API endpoints

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const {sharksList, catsList} = require('./data-files/lists');
 const PORT = 8080;
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.json({greetings: 'Hello World'});
+app.get('/api/cats', (req, res) => {
+  res.json(catsList);
 })
 
+app.get('/api/sharks', (req, res) => {
+  res.json(sharksList);
+})
 
+app.get('/api/random', (re, res) => {
+  
+})
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
